3ace7d5c769ef2a303423e7d76c894e4b5af38cf
rolstonr <richardrolston@aol.com>
2017-05-21 21:53:29 -0700

    Add files via upload

---
+++ module4-solution/routes.js
@@ -0,0 +1,54 @@
+/*
+Richard Rolston
+Coursera Class: Single Page Web Applications with AngularJs
+Module 4 Coding Assignment
+Instructor: Yaakov Chaikin
+Date: May 21, 2017
+*/
+(function() {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+     url: '/',
+     templateUrl: 'src/menu/templates/home.template.html'
+  })
+  // Category page
+  .state('categories', {
+   url: '/categories',
+   templateUrl: 'src/menu/templates/main-categories.template.html',
+   controller: 'CategoriesController as categoriesController',
+   resolve: {
+     categories: ['MenuDataService',
+                  function(MenuDataService) {
+                    return MenuDataService.getAllCategories();
+                  }
+                 ]
+   }
+  })
+  // Item detail page
+  .state('items', {
+   url: '/items/{short_name}/{category}',
+   templateUrl: 'src/menu/templates/main-items.template.html',
+   controller: 'ItemsController as itemsCtrl',
+   resolve: {
+   items: ['$stateParams', 'MenuDataService',
+            function($stateParams, MenuDataService) {
+              return MenuDataService.getItemsForCategory($stateParams.short_name);
+            }
+          ]
+   }
+  });
+}
+})();
